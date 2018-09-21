4bc107d2bd0048acdd4d7e9042b8784d1fb1480b
VillePajala <valoraami@gmail.com>
2018-09-21 11:46:03 +0300

    Minor changes

--- js/script.js
+++ js/script.js
@@ -4,23 +4,22 @@ $(document).ready(function(){
 
     $("#start_button").click(function(){
 
-
-
       $('#start_button').fadeOut("slow");
       $("#start_button").css("animation", "button_out 2s");
       $("#start_button").css("animation-fill-mode", "forwards");
       setTimeout(function(){
         nextBox();
-
-      }, 2000);
-    });
+      }, 1000);
+    }); // start_button
 
 
     function nextBox() {
-      var txt1 = $("<div id='next_button'><h3>Start<br>Here</h3></div>");
+      var txt1 = $("<div class='container' id='next_button'>");
+      var txt2 = $("<input>Name?</input>");
       // $("div").attr('id', 'next_button');
       $("body").append(txt1).fadeIn("slow");
+      $("#next_button").append(txt2).fadeIn("slow");
 
-    }
+    } // nextBox
 
 }); // Main
