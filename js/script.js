0a18e368c7f30ef323cb16070a9abe08ccb891e0
VillePajala <valoraami@gmail.com>
2018-09-21 11:04:49 +0300

    Minor improvements

--- js/script.js
+++ js/script.js
@@ -2,10 +2,25 @@
 
 $(document).ready(function(){
 
-  $("#start").click(function(){
-        $("h3").css("color", "white");
-        $("#start").css("width", "600");
+    $("#start_button").click(function(){
 
+
+
+      $('#start_button').fadeOut("slow");
+      $("#start_button").css("animation", "button_out 2s");
+      $("#start_button").css("animation-fill-mode", "forwards");
+      setTimeout(function(){
+        nextBox();
+
+      }, 2000);
     });
+
+
+    function nextBox() {
+      var txt1 = $("<div id='next_button'><h3>Start<br>Here</h3></div>");
+      // $("div").attr('id', 'next_button');
+      $("body").append(txt1).fadeIn("slow");
+
+    }
 
 }); // Main
